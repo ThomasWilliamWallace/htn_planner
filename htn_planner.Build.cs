49e19e2c977631722a211f5f6e670b7ace692abc
Thomas Wallace <trihardar@gmail.com>
2021-08-29 14:22:13 +0100

    Added unreal engine buildfile.

---
+++ htn_planner.Build.cs
@@ -0,0 +1,13 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class htn_planner : ModuleRules
+{
+	public htn_planner(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		bUseRTTI = true;
+	}
+}
